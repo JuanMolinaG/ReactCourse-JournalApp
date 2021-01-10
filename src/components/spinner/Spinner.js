0dccb19b5aecb4abcdb2e0b27ff55c5a3f75f054
Juan G. Molina <jg.molina92@hotmail.com>
2021-01-09 20:13:14 -0500

    Fix in property name

--- src/components/spinner/Spinner.js
+++ src/components/spinner/Spinner.js
@@ -2,12 +2,12 @@ import React from 'react';
 
 export const Spinner = () => {
   return (
-    <div class='spinner__container'>
-      <div class='spinner__rect1'></div>
-      <div class='spinner__rect2'></div>
-      <div class='spinner__rect3'></div>
-      <div class='spinner__rect4'></div>
-      <div class='spinner__rect5'></div>
+    <div className='spinner__container'>
+      <div className='spinner__rect1'></div>
+      <div className='spinner__rect2'></div>
+      <div className='spinner__rect3'></div>
+      <div className='spinner__rect4'></div>
+      <div className='spinner__rect5'></div>
     </div>
   );
 };
